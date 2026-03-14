88dcea0466855e57ce772a8be8accd3ebaebb95c
v17365409-wq <v17365409@gmail.com>
2026-03-14 16:05:03 +0530

    coded uc2 to print oops in    star formation

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,5 +1,15 @@
 class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     ");
+        System.out.println("*     * *     * *****   ***** ");
+        System.out.println("*     * *     * *            *");
+        System.out.println(" *****   *****  *       ***** ");
+
     }
-}
+}g    
+
+
+    
+
