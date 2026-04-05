6734f62c07ad409bedb26269b793ac8391e7a68e
v17365409-wq <v17365409@gmail.com>
2026-04-05 14:18:00 +0530

    uc-6 modular design using helper methods

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,20 +1,48 @@
 
 public class OOPSBannerApp {
 
+    // Method for O
+    public static String[] getO() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Method for P
+    public static String[] getP() {
+        return new String[]{
+            "***** ",
+            "*    *",
+            "***** ",
+            "*     ",
+            "*     "
+        };
+    }
+
+    // Method for S
+    public static String[] getS() {
+        return new String[]{
+            " *****",
+            "*     ",
+            " *****",
+            "     *",
+            "***** "
+        };
+    }
+
     public static void main(String[] args) {
 
-        // Directly create array using String.join()
-        String[] banner = {
-            String.join(" ", "*****", "*****", "*****", "*****"),
-            String.join(" ", "*   *", "*   *", "*   *", "*"),
-            String.join(" ", "*   *", "*   *", "*****", "*****"),
-            String.join(" ", "*   *", "*   *", "*", "*"),
-            String.join(" ", "*****", "*****", "*", "*****")
-        };
+        String[] o = getO();
+        String[] p = getP();
+        String[] s = getS();
 
-        // Enhanced for loop
-        for (String line : banner) {
-            System.out.println(line);
+        // Print O O P S line by line
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i]);
         }
     }
 }
